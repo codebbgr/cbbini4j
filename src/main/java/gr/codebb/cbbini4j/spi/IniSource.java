b20f4587cda25d1991bc9d0fdde31ece2374f344
georgemoralis <giorgosmrls@gmail.com>
2020-04-04 10:52:47 +0300

    Fix line separators processing in comments parsing logic

--- src/main/java/gr/codebb/cbbini4j/spi/IniSource.java
+++ src/main/java/gr/codebb/cbbini4j/spi/IniSource.java
@@ -5,6 +5,7 @@
 /**
  * Changelog
  * =========
+ * 04/04/2020 (gmoralis) - Fix line separators processing in comments parsing logic (jetbrains patch)
  * 03/04/2020 (gmoralis) - Initial commit from ini4j project
  */
 package gr.codebb.cbbini4j.spi;
@@ -18,8 +19,8 @@ import java.io.Reader;
 
 import java.net.URL;
 
-class IniSource
-{
+class IniSource {
+
     public static final char INCLUDE_BEGIN = '<';
     public static final char INCLUDE_END = '>';
     public static final char INCLUDE_OPTIONAL = '?';
@@ -31,54 +32,42 @@ class IniSource
     private final HandlerBase _handler;
     private final LineNumberReader _reader;
 
-    IniSource(InputStream input, HandlerBase handler, String comments, Config config)
-    {
+    IniSource(InputStream input, HandlerBase handler, String comments, Config config) {
         this(new UnicodeInputStreamReader(input, config.getFileEncoding()), handler, comments, config);
     }
 
-    IniSource(Reader input, HandlerBase handler, String comments, Config config)
-    {
+    IniSource(Reader input, HandlerBase handler, String comments, Config config) {
         _reader = new LineNumberReader(input);
         _handler = handler;
         _commentChars = comments;
         _config = config;
     }
 
-    IniSource(URL input, HandlerBase handler, String comments, Config config) throws IOException
-    {
+    IniSource(URL input, HandlerBase handler, String comments, Config config) throws IOException {
         this(new UnicodeInputStreamReader(input.openStream(), config.getFileEncoding()), handler, comments, config);
         _base = input;
     }
 
-    int getLineNumber()
-    {
+    int getLineNumber() {
         int ret;
 
-        if (_chain == null)
-        {
+        if (_chain == null) {
             ret = _reader.getLineNumber();
-        }
-        else
-        {
+        } else {
             ret = _chain.getLineNumber();
         }
 
         return ret;
     }
 
-    String readLine() throws IOException
-    {
+    String readLine() throws IOException {
         String line;
 
-        if (_chain == null)
-        {
+        if (_chain == null) {
             line = readLineLocal();
-        }
-        else
-        {
+        } else {
             line = _chain.readLine();
-            if (line == null)
-            {
+            if (line == null) {
                 _chain = null;
                 line = readLine();
             }
@@ -87,66 +76,54 @@ class IniSource
         return line;
     }
 
-    private void close() throws IOException
-    {
+    private void close() throws IOException {
         _reader.close();
     }
 
-    private int countEndingEscapes(String line)
-    {
+    private int countEndingEscapes(String line) {
         int escapeCount = 0;
 
-        for (int i = line.length() - 1; (i >= 0) && (line.charAt(i) == ESCAPE_CHAR); i--)
-        {
+        for (int i = line.length() - 1; (i >= 0) && (line.charAt(i) == ESCAPE_CHAR); i--) {
             escapeCount++;
         }
 
         return escapeCount;
     }
 
-    private void handleComment(StringBuilder buff)
-    {
-        if (buff.length() != 0)
-        {
-            buff.deleteCharAt(buff.length() - 1);
+    private void handleComment(StringBuilder buff) {
+        if (buff.length() != 0) {
+            String lineSeparator = _config.getLineSeparator();
+
+            if (lineSeparator.length() <= buff.length()) {
+                buff.delete(buff.length() - lineSeparator.length(), buff.length());
+            }
             _handler.handleComment(buff.toString());
             buff.delete(0, buff.length());
         }
     }
 
-    private String handleInclude(String input) throws IOException
-    {
+    private String handleInclude(String input) throws IOException {
         String line = input;
 
-        if (_config.isInclude() && (line.length() > 2) && (line.charAt(0) == INCLUDE_BEGIN) && (line.charAt(line.length() - 1) == INCLUDE_END))
-        {
+        if (_config.isInclude() && (line.length() > 2) && (line.charAt(0) == INCLUDE_BEGIN) && (line.charAt(line.length() - 1) == INCLUDE_END)) {
             line = line.substring(1, line.length() - 1).trim();
             boolean optional = line.charAt(0) == INCLUDE_OPTIONAL;
 
-            if (optional)
-            {
+            if (optional) {
                 line = line.substring(1).trim();
             }
 
             URL loc = (_base == null) ? new URL(line) : new URL(_base, line);
 
-            if (optional)
-            {
-                try
-                {
+            if (optional) {
+                try {
                     _chain = new IniSource(loc, _handler, _commentChars, _config);
-                }
-                catch (IOException x)
-                {
+                } catch (IOException x) {
                     assert true;
-                }
-                finally
-                {
+                } finally {
                     line = readLine();
                 }
-            }
-            else
-            {
+            } else {
                 _chain = new IniSource(loc, _handler, _commentChars, _config);
                 line = readLine();
             }
@@ -155,45 +132,33 @@ class IniSource
         return line;
     }
 
-    private String readLineLocal() throws IOException
-    {
+    private String readLineLocal() throws IOException {
         String line = readLineSkipComments();
 
-        if (line == null)
-        {
+        if (line == null) {
             close();
-        }
-        else
-        {
+        } else {
             line = handleInclude(line);
         }
 
         return line;
     }
 
-    private String readLineSkipComments() throws IOException
-    {
+    private String readLineSkipComments() throws IOException {
         String line;
         StringBuilder comment = new StringBuilder();
         StringBuilder buff = new StringBuilder();
 
-        for (line = _reader.readLine(); line != null; line = _reader.readLine())
-        {
+        for (line = _reader.readLine(); line != null; line = _reader.readLine()) {
             line = line.trim();
-            if (line.length() == 0)
-            {
+            if (line.length() == 0) {
                 handleComment(comment);
-            }
-            else if ((_commentChars.indexOf(line.charAt(0)) >= 0) && (buff.length() == 0))
-            {
+            } else if ((_commentChars.indexOf(line.charAt(0)) >= 0) && (buff.length() == 0)) {
                 comment.append(line.substring(1));
                 comment.append(_config.getLineSeparator());
-            }
-            else
-            {
+            } else {
                 handleComment(comment);
-                if (!_config.isEscapeNewline() || ((countEndingEscapes(line) & 1) == 0))
-                {
+                if (!_config.isEscapeNewline() || ((countEndingEscapes(line) & 1) == 0)) {
                     buff.append(line);
                     line = buff.toString();
 
@@ -205,8 +170,7 @@ class IniSource
         }
 
         // handle end comments
-        if ((line == null) && (comment.length() != 0))
-        {
+        if ((line == null) && (comment.length() != 0)) {
             handleComment(comment);
         }
 
